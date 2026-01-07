6435d30e478c89b562d26cbcf039ab226c6d143b
Ashmit Das <dasashmit16@gmail.com>
2026-01-07 18:12:58 +0530

    upload the python script to use the model

--- main.py
+++ main.py
@@ -1,5 +1,75 @@
+import torch
+from untrained_model import GPTModel
+import tiktoken
+from untrained_model import generate
+from untrained_model import text_to_token_ids
+from untrained_model import token_ids_to_text
+
+def format_input(entry):
+    instruction_text = f"""Below is an instruction that describes a task. Write a response that appropriately completes the request. 
+    
+### Instruction:\n{entry['instruction']}
+    """
+
+    return instruction_text
+
 def main():
-    print("Hello from llm-from-scratch!")
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,   # Vocabulary size
+        "context_length": 256, # Shortened context length (orig: 1024)
+        "emb_dim": 256,        # Embedding dimension
+        "n_heads": 4,         # Number of attention heads
+        "n_layers": 12,        # Number of layers
+        "drop_rate": 0.1,      # Dropout rate
+        "qkv_bias": False      # Query-key-value bias
+    }
+
+    checkpoint = torch.load("model_and_optimizer_small.pth", weights_only=True)
+
+    model = GPTModel(GPT_CONFIG_124M)
+    model.load_state_dict(torch.load("sarcasm_finetuned_v1.pth"))
+    model.eval();
+
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+
+
+    inference_device = torch.device("cpu")
+
+    model.to(inference_device)
+
+    torch.manual_seed(123)
+    model.to(inference_device)
+    model.eval()
+
+    input_query = input("Enter query: ")
+
+    input_text = format_input({"instruction":input_query})
+
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer).to(inference_device),
+        max_new_tokens=30,
+        context_size=GPT_CONFIG_124M["context_length"],
+        top_k=10,
+        temperature=0.9
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+
+    response_text = (
+        generated_text[len(input_text):]
+        .replace("### Response:", "")
+        .replace("###", "")
+        .replace("Response:", "")
+        .replace("Response", "")
+        .replace("<|endoftext|>", "")
+        .strip()
+    )
+    print( response_text)
+
+
+
 
 
 if __name__ == "__main__":
